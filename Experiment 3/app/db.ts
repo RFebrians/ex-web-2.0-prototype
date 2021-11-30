4c74eef36ac31616108620cb5e4bb083927803ba
R. Febriansyah <71978432+RFebrians@users.noreply.github.com>
2021-11-30 13:46:13 +0700

    fix bug

--- Experiment 3/app/db.ts
+++ Experiment 3/app/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL || "https://vkriqcdehjjwtzueuscm.supabase.co";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzkzNDQ0NiwiZXhwIjoxOTUzNTEwNDQ2fQ.q-mvXRaDA6YpnJlqZ5a-DFF911hLN8zleu5e1ypAd_w";
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
